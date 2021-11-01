8ce356c3b9a39ad30c74f9f9aabce35f1b3fd903
cmontana <cmontana@skandia.com.co>
2021-11-01 15:06:38 -0500

    Fin del curso

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('mi app');
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const { logErrors, errrorHandler, boomErrorHandler } = require('./middlewares/error.handler')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = [ 'http://localhost:8080', 'https://myapp.co', 'http://localhost:3000', undefined ];
+const options = {
+  origin: (origin, callback) => {
+    console.log(origin);
+    if(whitelist.includes(origin)){
+      callback(null, true);
+    }
+    else{
+      callback(new Error('no permitido'))
+    }
+  }
+}
+
+app.use(cors(options));
+//app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errrorHandler);
+
+app.listen(port, () => {
+  console.log('Mi port ' + port);
+});
